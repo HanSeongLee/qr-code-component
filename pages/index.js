c56b04c1e849db140bd673f20656fbee5977deec
HanSeong Lee <jkhanseong@gmail.com>
2022-01-21 22:49:13 +0900

    Finished

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,67 @@
+import styles from './style.module.scss';
 import Head from 'next/head';
+import QRCard from "../components/QRCard";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport"
-              content="width=device-width, initial-scale=1.0"
-        />
+      <>
+        <Head>
+          <meta charSet="UTF-8"/>
+          <meta name="viewport"
+                content="width=device-width, initial-scale=1.0"
+          />
 
-        <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-        <meta name="description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+          <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+          <meta name="description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
 
-        <meta name="description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+          <meta name="description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
 
-        <meta property="og:url"
-              content={process.env.NEXT_PUBLIC_URL}
-        />
-        <meta property="og:type"
-              content="website"
-        />
-        <meta property="og:title"
-              content={process.env.NEXT_PUBLIC_TITLE}
-        />
-        <meta property="og:description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
-        <meta property="og:image"
-              content={process.env.NEXT_PUBLIC_OG_IMAGE}
-        />
+          <meta property="og:url"
+                content={process.env.NEXT_PUBLIC_URL}
+          />
+          <meta property="og:type"
+                content="website"
+          />
+          <meta property="og:title"
+                content={process.env.NEXT_PUBLIC_TITLE}
+          />
+          <meta property="og:description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
+          <meta property="og:image"
+                content={process.env.NEXT_PUBLIC_OG_IMAGE}
+          />
 
-        <meta name="twitter:card"
-              content="summary_large_image"
-        />
-        <meta property="twitter:url"
-              content={process.env.NEXT_PUBLIC_URL}
-        />
-        <meta name="twitter:title"
-              content={process.env.NEXT_PUBLIC_TITLE}
-        />
-        <meta name="twitter:description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
-        <meta name="twitter:image"
-              content={process.env.NEXT_PUBLIC_OG_IMAGE}
-        />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-      </Head>
+          <meta name="twitter:card"
+                content="summary_large_image"
+          />
+          <meta property="twitter:url"
+                content={process.env.NEXT_PUBLIC_URL}
+          />
+          <meta name="twitter:title"
+                content={process.env.NEXT_PUBLIC_TITLE}
+          />
+          <meta name="twitter:description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
+          <meta name="twitter:image"
+                content={process.env.NEXT_PUBLIC_OG_IMAGE}
+          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+        </Head>
 
-      <main>
-      </main>
-    </>
-  )
+        <main>
+          <h1 className={styles.hidden}>
+            QR Code Component
+          </h1>
+          <div className={styles.container}>
+            <QRCard/>
+          </div>
+        </main>
+      </>
+  );
 }
